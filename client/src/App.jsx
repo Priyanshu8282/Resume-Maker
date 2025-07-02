c4254c1eade4d576bfbc610e675a41c3c7cd7525
PRIYANSHU SHARMA <140371901+Priyanshu8282@users.noreply.github.com>
2025-07-02 12:43:27 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./Component/Navbar/Navbar";
 import RegisterPage from "./Component/Auth/RegisterPage";
 import LoginPage from "./Component/Auth/LoginPage";
 import MainPage from "./Component/ResumePage/MainPage";
 import ResumePage from "./Component/ResumePage/ResumePage";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "./Component/Auth/AuthContext";
 
+function ProtectedRoute({ children }) {
+  const { isLogged } = useContext(AuthContext);
+  if (!isLogged) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
+
+function PublicRoute({ children }) {
+  const { isLogged } = useContext(AuthContext);
+  if (isLogged) {
+    return <Navigate to="/resume" replace />;
+  }
+  return children;
+}
 
 function App() {
   return (
@@ -15,11 +30,10 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/resume" element={<MainPage/>} />
-        <Route path="/resume/:id" element={<ResumePage />} />
-
+        <Route path="/" element={<PublicRoute><LoginPage /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
+        <Route path="/resume" element={<ProtectedRoute><MainPage/></ProtectedRoute>} />
+        <Route path="/resume/:id" element={<ProtectedRoute><ResumePage /></ProtectedRoute>} />
       </Routes>
     
     
